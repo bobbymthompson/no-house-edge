319317fef8564d54ef631f6e414adae68ad43338
Bobby Thompson <bobby.m.thompson@gmail.com>
2017-03-11 16:42:42 -0500

    Update for deploying to Heroku

--- server.ts
+++ server.ts
@@ -33,6 +33,8 @@ app.use(function (req, res, next) {
   next();
 });
 
+console.log('Starting server');
+
 // listen (start app with node server.js) ======================================
 app.listen(app.get('port'));
 console.log("App listening on port " + app.get('port'));
